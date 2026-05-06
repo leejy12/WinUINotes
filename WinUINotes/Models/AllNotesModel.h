1e77e19996a157faaa4c7f742040d33490d23eae
Junyoung Lee <60320918+leejy12@users.noreply.github.com>
2026-05-06 15:57:53 -0500

    4. All notes (#2)

---
+++ WinUINotes/Models/AllNotesModel.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Models/NoteModel.h"
+
+#include "Models/AllNotesModel.g.h"
+
+namespace winrt::WinUINotes::Models::implementation
+{
+
+struct AllNotesModel : AllNotesModelT<AllNotesModel>
+{
+  private:
+    winrt::Windows::Foundation::Collections::IObservableVector<winrt::WinUINotes::Models::NoteModel> notes;
+
+    winrt::Windows::Foundation::IAsyncAction LoadNotes();
+    winrt::Windows::Foundation::IAsyncAction GetFilesInFolderAsync(winrt::Windows::Storage::StorageFolder folder);
+
+  public:
+    AllNotesModel();
+
+    winrt::Windows::Foundation::Collections::IObservableVector<winrt::WinUINotes::Models::NoteModel> Notes()
+    {
+        return notes;
+    }
+
+    void Notes(const winrt::Windows::Foundation::Collections::IObservableVector<winrt::WinUINotes::Models::NoteModel> &_notes)
+    {
+        notes = _notes;
+    }
+};
+} // namespace winrt::WinUINotes::Models::implementation
+
+namespace winrt::WinUINotes::Models::factory_implementation
+{
+struct AllNotesModel : AllNotesModelT<AllNotesModel, implementation::AllNotesModel>
+{
+};
+} // namespace winrt::WinUINotes::Models::factory_implementation
---
+++ WinUINotes/Views/AllNotesPage.xaml.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "AllNotesPage.xaml.h"
+#if __has_include("Views/AllNotesPage.g.cpp")
+#include "Views/AllNotesPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::WinUINotes::Views::implementation
+{
+}
