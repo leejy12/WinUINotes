2b7caae53f9d6391b50d8d03ccce49eb20ab6340
Junyoung Lee <leejy31415@outlook.com>
2026-05-06 14:40:16 -0500

    implement allnotespage

--- WinUINotes/Views/AllNotesPage.xaml.h
+++ WinUINotes/Views/AllNotesPage.xaml.h
@@ -1,22 +1,38 @@
 #pragma once
 
+#include "Models/AllNotesModel.h"
+
 #include "Views/AllNotesPage.g.h"
 
 namespace winrt::WinUINotes::Views::implementation
 {
-    struct AllNotesPage : AllNotesPageT<AllNotesPage>
+struct AllNotesPage : AllNotesPageT<AllNotesPage>
+{
+  public:
+    winrt::WinUINotes::Models::AllNotesModel allNotes;
+
+  public:
+    AllNotesPage()
     {
-        AllNotesPage()
-        {
-            // Xaml objects should not call InitializeComponent during construction.
-            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
-        }
-    };
-}
+        // Xaml objects should not call InitializeComponent during construction.
+        // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+    }
+
+    winrt::WinUINotes::Models::AllNotesModel AllNotes()
+    {
+        return allNotes;
+    }
+
+    void AllNotes(const winrt::WinUINotes::Models::AllNotesModel &_allNotes)
+    {
+        allNotes = _allNotes;
+    }
+};
+} // namespace winrt::WinUINotes::Views::implementation
 
 namespace winrt::WinUINotes::Views::factory_implementation
 {
-    struct AllNotesPage : AllNotesPageT<AllNotesPage, implementation::AllNotesPage>
-    {
-    };
-}
+struct AllNotesPage : AllNotesPageT<AllNotesPage, implementation::AllNotesPage>
+{
+};
+} // namespace winrt::WinUINotes::Views::factory_implementation
