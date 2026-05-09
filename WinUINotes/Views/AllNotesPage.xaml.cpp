3457b732f85d5a4b44e8ba07c7d02fed4100bfc6
Junyoung Lee <leejy31415@outlook.com>
2026-05-08 20:55:12 -0500

    (WIP) delete note from context menu

--- WinUINotes/Views/AllNotesPage.xaml.cpp
+++ WinUINotes/Views/AllNotesPage.xaml.cpp
@@ -7,6 +7,7 @@
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Microsoft::UI::Xaml;
 using namespace winrt::Microsoft::UI::Xaml::Controls;
+using namespace winrt::WinUINotes::Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -18,10 +19,21 @@ void AllNotesPage::NewNoteButton_Click(const IInspectable &sender, const RoutedE
     Frame().Navigate(winrt::xaml_typename<NotePage>());
 }
 
-void AllNotesPage::ItemsView_ItemInvoked(const ItemsView &sender,
-                                                         const ItemsViewItemInvokedEventArgs &args)
+void AllNotesPage::ItemsView_ItemInvoked(const ItemsView &sender, const ItemsViewItemInvokedEventArgs &args)
 {
     Frame().Navigate(winrt::xaml_typename<NotePage>(), args.InvokedItem());
+}
+
+void AllNotesPage::NoteMenuFlyoutItem_Click(const IInspectable &sender, const RoutedEventArgs &e)
+{
+    const MenuFlyoutItem item = sender.as<MenuFlyoutItem>();
+    const winrt::hstring fileName = item.Tag().as<winrt::hstring>();
+    const auto &notes = allNotes.Notes();
+    const auto it = std::ranges::find_if(notes, [&fileName](const NoteModel &n) { return n.FileName() == fileName; });
+    const std::uint32_t idx = static_cast<std::uint32_t>(std::distance(notes.begin(), it));
+    notes.RemoveAt(idx);
+
+    // TODO: Actually delete the file.
 }
 
 } // namespace winrt::WinUINotes::Views::implementation
